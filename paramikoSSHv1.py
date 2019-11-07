669c60a9f992e5134a925d106626762373988818
Noelia <40467401+Lestrigones@users.noreply.github.com>
2019-11-07 09:41:24 +0100

    Update paramikoSSHv1.py

--- paramikoSSHv1.py
+++ paramikoSSHv1.py
@@ -1,25 +1,32 @@
 import paramiko
 import time
 
+
+
+#datos del router
 ip_address="10.10.10.10"
 username="cisco123"
 password="cisco123"
 
+#para establecer la conexión
 ssh_client = paramiko.SSHClient()
 ssh_client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 ssh_client.connect(hostname=ip_address,username=username,password=password)
 
 
-print ("Successful connection", ip_address)
+print ("Conexión establecida con: ", ip_address)
 
-
+#llamamos a consola y realizamos las peticiones con los comandos
 remote_connection = ssh_client.invoke_shell()
+remote_connection.send(b'enable \n')
+remote_connection.send(b'configure terminal \n')
+remote_connection.send(b'int loop 0 \n')
+remote_connection.send(b'ip address 1.1.1.1 255.255.255.255 \n')
+remote_connection.send(b'do show run  \n')
+time.sleep(3)
 
-remote_connection.send("enable \n")
-remote_connection.send("configure terminal \n")
-remote_connection.send("int loop 0 ")
-remote_connection.send("ip address 1.1.1.1 255.255.255.255 \n")
-
+#metemos la info recogida recv para leer y printarlo, sockets
 output = remote_connection.recv(65535)
-salida=remote_connection.send("do show ip run \n")
-print (output)
+print (output.decode('ascii'))
+
+ssh_client.close()
