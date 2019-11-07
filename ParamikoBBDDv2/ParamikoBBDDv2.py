2144cf222b33c7449d267df3b128967be8a6f800
Noelia <40467401+Lestrigones@users.noreply.github.com>
2019-11-07 10:56:53 +0100

    Add files via upload

---
+++ ParamikoBBDDv2/ParamikoBBDDv2.py
@@ -0,0 +1,43 @@
+from netmiko import ConnectHandler
+from netmiko.ssh_exception import NetMikoTimeoutException
+from paramiko.ssh_exception import SSHException
+from netmiko.ssh_exception import AuthenticationException
+import time
+import datetime
+
+
+#cadenas de tiempo para poder guardar el archivo con la fecha exacta y la hora
+TiempoActual= datetime.datetime.now().replace(microsecond=0)
+
+#lista de las ip de los routers
+ip_lista = open('ListaIPS')
+
+#recorrido para que pueda lea todas las ip del archivo, damos por hecho q el username, pass son iguales para todos
+for ip in ip_lista:
+    print ('\n  '+ ip.strip() + ' \n')
+    router = {
+    'ip':   ip,
+    'username': 'python',
+    'password': 'cisco',
+    'device_type': 'cisco_ios',
+    }
+##creamos una excepciones, conectado, no conectado, creacion de la bbdd en txt
+    try:
+        net_connect = ConnectHandler(**router)#llamamos a los datos
+    except NetMikoTimeoutException:
+        print ('Dispositivo no accesible.')
+        continue
+    except AuthenticationException:
+        print ('Fallo de autenticación.')
+        continue
+    except SSHException:
+        print ('Creación de conexión por SSh en el dispositivo.')
+        continue
+
+    print ('Iniciando el salvado de datos en la BBDD')
+    salida = net_connect.send_command('show run')
+##creamos el fichero con el nombre de la ip del router
+    fsaved = open("router_" + ip, 'w')
+    fsaved.write(salida)
+    fsaved.close
+    print ('Finalizada la BBDD')
